8f6d11660a9c17bacf5c286fe1444fbad25dfc17
= <tigran_grigoryan05@inbox.ru>
2023-06-07 20:51:44 +0400

    Added Admin controller/Service/Repository

--- src/main/java/com/ordering_system/service/impl/AdminServiceImpl.java
+++ src/main/java/com/ordering_system/service/impl/AdminServiceImpl.java
@@ -1,34 +1,66 @@
 package com.ordering_system.service.impl;
 
+import com.ordering_system.model.domain.AdminEntity;
 import com.ordering_system.model.dto.Admin;
+import com.ordering_system.repository.AdminRepository;
+import com.ordering_system.repository.FoodRepository;
 import com.ordering_system.service.AdminService;
+import com.ordering_system.service.converter.Converter;
+import com.ordering_system.service.validator.Validator;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 @Service
 public class AdminServiceImpl implements AdminService {
+	
+	private final AdminRepository adminRepository;
+	private final Converter converter;
+	
+	@Autowired
+	public AdminServiceImpl(AdminRepository adminRepository, Converter converter) {
+		this.adminRepository = adminRepository;
+		this.converter = converter;
+	}
+	
     @Override
     public Admin getById(long id) {
-        return null;
+        Validator.checkId(id);
+        Validator.checkEntity(adminRepository.findAdminEntityById(id));
+        return converter.entityToAdmin(adminRepository.findAdminEntityById(id));
     }
 
     @Override
     public List<Admin> getAll() {
-        return null;
+        return converter.entityToAdminList(adminRepository.findAll());
     }
 
     @Override
     public Admin save(Admin admin) {
-        return null;
-    }
+    	Validator.checkEntity(admin);
+    	adminRepository.save(converter.adminToEntity(admin));
+    	return admin;
+     }
 
     @Override
-    public Admin update(Admin admin) {
-        return null;
+    public Admin update(long id,Admin admin) {
+    	AdminEntity adminEntity = adminRepository.findAdminEntityById(id);
+    	Validator.checkEntity(admin);
+    	if(admin.getPhoneNumber() != null) {
+    		adminEntity.setPhoneNumber(admin.getPhoneNumber());
+    	}
+    	if(admin.getPassword() != null) {
+    		adminEntity.setPassword(admin.getPassword());
+    	}
+    	adminRepository.save(converter.adminToEntity(admin));
+    	return admin;
     }
 
     @Override
     public void delete(long id) {
-
+    	Validator.checkId(id);
+    	Validator.checkEntity(adminRepository.findAdminEntityById(id));
+    	adminRepository.deleteById(id);
     }
 }
